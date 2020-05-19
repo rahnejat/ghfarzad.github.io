f2d4b654bc9afe5302c4e52de5795e3d10e3acd5
Farzad Ghaznavi <ghfarzad@gmail.com>
2020-05-19 13:23:59 -0400

    WIP

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,17 +1,17 @@
 console.log("Hello RE!");
 
-//let anft = document.querySelector("a-nft");
-//anft.addEventListener(
-//    "markerFound",
-//    event => {
-//        window.alert("Marker Found!");
-//    }
-//)
-//
-//let button = document.querySelector("button");
-//button.addEventListener(
-//    "click",
-//    event => {
-//        window.alert("Button Clicked!");
-//    }
-//)
+let anft = document.querySelector("a-nft");
+anft.addEventListener(
+    "markerFound",
+    event => {
+        window.alert("Marker Found!");
+    }
+)
+
+let button = document.querySelector("button");
+button.addEventListener(
+    "click",
+    event => {
+        window.alert("Button Clicked!");
+    }
+)
